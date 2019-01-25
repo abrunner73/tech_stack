328bc73f622075c57c745853615a9b89c3da057f
Andrew Brunner <andrew.brunner@froedtert.com>
2019-01-25 17:03:08 -0600

    commit all files from last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import { View } from './src/App';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
 
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View>
+                <Header headerText="Tech Stack" />
+            </View>
+        </Provider>
+    );
+};
 
-AppRegistry.registerComponent('auth', () => App);
+export default App;
+
